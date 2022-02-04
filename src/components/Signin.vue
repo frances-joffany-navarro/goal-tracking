ad974e2e20fd1cd407c8c6d6760fbaddf41bbd97
Frances Joffany Navarro <francesjoffanynavarro@gmail.com>
2022-02-04 17:48:22 +0100

    add signin component

---
+++ src/components/Signin.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <form name="create-user-account" method="POST" data-netlify="true">
+      <p>
+        <label>First Name: <input type="text" name="firstname" /></label>
+      </p>
+      <p>
+        <label>Last Name: <input type="text" name="lastname" /></label>
+      </p>
+      <p>
+        <label>Your Email: <input type="email" name="email" /></label>
+      </p>
+      <p>
+        <label>Password: <input type="password" name="password" /></label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
