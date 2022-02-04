b08204294b588ec918e7c7ee53493ccd24f8726c
Frances Joffany Navarro <francesjoffanynavarro@gmail.com>
2022-02-04 15:31:16 +0100

    import fontawesome

--- src/main.js
+++ src/main.js
@@ -3,6 +3,18 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 import router from './router'
 
+//StyleSheet
+import '@/assets/style.css';
+
+//FontAwesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSeedling } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faSeedling)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 Vue.use(VueRouter)
 
